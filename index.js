5b95861f3c55b8d98185b85227f8c65eab163ecc
pitamchandra <pitamchandra402@gmail.com>
2023-05-02 20:20:30 +0600

    install cors and setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const chefs = require('./data/chefs.json')
+const app = express()
 const port = process.env.PORT || 5000;
 
+
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('my server is running...')
